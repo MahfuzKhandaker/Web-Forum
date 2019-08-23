77e72f1c98229644cad43a335609ab4fd874db7f
MahfuzKhandaker <mahfuzkhandaker.bd@gmail.com>
2019-08-24 01:10:11 +0600

    commit file change

--- forumproject/settings.py
+++ forumproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webforum.apps.WebforumConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

984c0fb1a2b7827b94139b35969e71ee10b7c734
MahfuzKhandaker <mahfuzkhandaker.bd@gmail.com>
2019-08-28 23:40:41 +0600

    new coomit

--- webforum/urls.py
+++ webforum/urls.py
@@ -4,4 +4,5 @@ from webforum import views
 urlpatterns=[
     path('', views.home, name='home'),
     path('forums/<int:pk>/', views.forum_topics, name='forum_topics'),
+    path('forums/<int:pk>/new/', views.new_topic, name='new_topic'),
 ]
--- webforum/views.py
+++ webforum/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from webforum.models import Forum
-from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from webforum.models import Forum, Topic, Post
+from django.contrib.auth.models import User
+from .forms import NewTopicForm 
 
 def home(request):
     forums = Forum.objects.all()
@@ -9,5 +10,34 @@ def home(request):
 
 def forum_topics(request, pk):
     forum = get_object_or_404(Forum, pk=pk) 
-    return render(request, 'topics.html', {'forum': forum})
+    contex = {
+        'forum':forum
+    }
+    return render(request, 'topics.html', contex)
 
+
+def new_topic(request, pk):
+    forum = get_object_or_404(Forum, pk=pk)
+    user = User.objects.first()
+
+    form = NewTopicForm()
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.forum = forum
+            topic.started_by = user
+            topic.save()
+            
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('forum_topics', pk=forum.pk)
+
+    else:
+        form = NewTopicForm()
+
+    context = {'forum':forum, 'form':form}
+    return render(request, 'new_topic.html', context )

bd26e928e2e5359d63d4ea2df02ffdc67df3bdb1
MahfuzKhandaker <mahfuzkhandaker.bd@gmail.com>
2019-09-01 13:22:00 +0600

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from accounts import views as accounts_view
+from django.contrib.auth import views as auth_views
 
-urlpatterns=[
+urlpatterns = [
     path('signup/', accounts_view.signup, name='signup'),
+    path('loging/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.contrib.auth import login as auth_login
 
 def signup(request):
-    form = UserCreationForm()
-        
+    form = SignUpForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('home')
             
         else:
-            form = UserCreationForm()
+            form = SignUpForm()
 
     context = {
         'form':form
